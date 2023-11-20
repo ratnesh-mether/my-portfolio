b9ac5d06845268f7b43aeff0e420b983a31b03c5
Ratnesh Mether <mywork.ratnesh@gmail.com>
2023-11-20 23:50:33 +0530

    [5]: Parallax Effect Base.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,30 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio Starting Soon...</h1>
+      <div className="parallax_wrapper">
+        <div className="parallax_group intro_screen" id="intro">
+          Parallax Demo
+        </div>
+        <div className="parallax_group" id="group-1">
+          <div className="parallax_layer base_layer">
+            Base Layer Page
+          </div>
+          <div className="parallax_layer mid_layer">
+            Mid Layer Page
+          </div>
+        </div>
+        <div className="parallax_group" id="group-2" >
+          <div className="parallax_layer mid_layer">
+            Mid Layer Page
+          </div>
+          <div className="parallax_layer top_layer">
+            Top Layer Page
+          </div>
+        </div>
+        <div className="parallax_group outro_screen" id="outro">
+          The End
+        </div>
+      </div>
     </div>
   );
 }
