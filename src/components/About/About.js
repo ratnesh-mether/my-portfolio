19dd1fe6d8240717e0b6cb1aa13a3c4521de32f7
Ratnesh Mether <mywork.ratnesh@gmail.com>
2023-12-03 21:03:08 +0530

    [12] : Theme Updates.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,12 @@
 import './../About/About.scss';
+import profile_picture from './../../assets/profile-pic.png';
 
 function About() {
     return <main className='about-component'>
         <h1>ABOUT</h1>
+        <div className="profile-image-container">
+            <img src={profile_picture} alt="profile" />
+        </div>
     </main>
 }
 
