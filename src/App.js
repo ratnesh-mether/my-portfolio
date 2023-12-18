051bf9867f29ec971991be77a109b97e9f97ccf5
Ratnesh Mether <mywork.ratnesh@gmail.com>
2023-12-18 12:10:29 +0530

    [25] : Layout Changes.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Home from './pages/Home/Home';
 import Header from './components/Header/Header';
 function App() {
   return (
-    <>
+    <main className='app-component'>
       <Header></Header>
       <Home></Home>
       <footer></footer>
-    </>
+    </main>
   );
 }
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -24,48 +24,48 @@ function Home() {
     }
     return <main className="home-component">
         {handlePDFView()}
-        <div className="scroll-helper" id="home"></div>
+        {/* <div className="scroll-helper" id="home"></div>
         <section className="base welcome-section">
-            <Welcome></Welcome>
+            <Welcome></Welcome> */}
 
-            {/* 
+        {/* 
     Intro text for visitor and popup to take the name
     */}
-        </section>
-        <div className="scroll-helper" id="about"></div>
+        {/* </section> */}
+        {/* <div className="scroll-helper" id="about" ></div> */}
         <section className="base about-section" >
             <About />
             {/* 
         photo / intro text / CTA - Email, Resume / Skills / LinkedIn / Twitter
         */}
         </section>
-        <div className="scroll-helper" id="projects"></div>
+        {/* <div className="scroll-helper" id="projects"></div> */}
         <section className="base project-section">
             <div className="inner">
                 <h1>Projects</h1>
             </div>
 
         </section>
-        <div className="scroll-helper" id="education"></div>
+        {/* <div className="scroll-helper" id="education"></div> */}
         <section className="base education-section">
             <div className="inner">
                 <h1>Education</h1>
             </div>
             <div className="scroll-helper"></div>
         </section>
-        <div className="scroll-helper" id="contact"></div>
+        {/* <div className="scroll-helper" id="contact"></div> */}
         <section className="base contact-section">
             <div className="inner">
                 <h1>Contact</h1>
             </div>
-            <div className="scroll-helper"></div>
+            {/* <div className="scroll-helper"></div> */}
         </section>
         <section className="base outro-section">
             <div className="inner">
                 <h1>Outro</h1>
             </div>
         </section>
-    </main>;
+    </main >;
 }
 
 export default Home;
