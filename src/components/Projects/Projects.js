9ca95940cd9955a0afb417c0428e3ff8455bb7f5
Ratnesh Mether <mywork.ratnesh@gmail.com>
2023-12-20 13:29:02 +0530

    [30] : Mobile Device Feature Update.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './Projects.scss'
 import { PROJECTS_DATA } from '../../utilities/Constants';
 
 function Projects() {
     const [loadMore, setLoadMore] = useState(true);
+    const [loadMorePoint, setLoadMorePoint] = useState(() => {
+        const emptyArray = Array(5).fill(true);
+        return emptyArray;
+    });
     const loadMoreHandler = () => {
         setLoadMore(!loadMore)
     }
-    const renderProject = (project) => {
+    const loadMorePointHandler = (flag, index) => {
+        const data = [...loadMorePoint]
+        data[index] = flag ? true : false;
+        setLoadMorePoint([...data]);
+    }
+    useEffect(() => {
+        console.log(loadMorePoint)
+    }, [loadMorePoint])
+    const renderProject = (project, projectIndex) => {
         return <div className="project-info-container" key={project.title}>
             <a href={project.url} target='_blank' rel="noreferrer">
                 <h2>{project.title} &#128279;</h2>
@@ -19,26 +31,23 @@ function Projects() {
                 <div className="info-text">
                     <ul>
                         {
-                            project.points.map((point) => {
-                                return <li>{point}</li>
+                            project.points.slice(0, loadMorePoint[projectIndex] ? 1 : project.points.length).map((point, index) => {
+                                return <li key={index}>{point}</li>
                             })
                         }
                     </ul>
-
+                    <p onClick={() => loadMorePointHandler(false, projectIndex)}>Show More</p>
+                    <p onClick={() => loadMorePointHandler(true, projectIndex)}>Show Less</p>
                 </div>
             </div>
         </div>
     }
     const projectList = () => {
-        return PROJECTS_DATA.map((project, index) => {
-            if (loadMore) {
-                if (index < 3)
-                    return renderProject(project);
-            }
-            else {
-                return renderProject(project);
-            }
-        })
+        return PROJECTS_DATA
+            .slice(0, loadMore ? 3 : PROJECTS_DATA.length - 1)
+            .map((project, index) => {
+                return renderProject(project, index);
+            })
     }
     return <main className='projects-component'>
         {projectList()}
