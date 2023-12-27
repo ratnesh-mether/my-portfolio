8a162cabd9401bf099edbf8588bfd854f0d8d53a
Ratnesh Mether <mywork.ratnesh@gmail.com>
2023-12-27 17:33:19 +0530

    Draft-1[5]: Animations Added.

--- src/components/Experiance/Experiance.js
+++ src/components/Experiance/Experiance.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './Experiance.scss'
 import { EXPERIANCE } from "../../utilities/Constants";
+import Aos from "aos";
+import 'aos/dist/aos.css';
 
 function Experiance() {
+    useEffect(() => {
+        Aos.init();
+    }, [])
     const showExperiance = () => {
-        return EXPERIANCE.map((exp) => {
-            return <div className="experiance" key={exp.name}>
+        return EXPERIANCE.map((exp, index) => {
+            const data = index % 2 === 0 ? "fade-right" : "fade-left";
+            return <div className="experiance" key={exp.name} data-aos={data} data-aos-offset='270'>
                 <div className="column org">
                     <a href={exp.link} target="_blank" rel="noreferrer">
                         <div className="logo" style={{ backgroundImage: "url(" + exp.img + ")" }}>
